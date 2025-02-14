b8eb439d122c09e44f6f37e5adaf46f2f1469eac
Randall Smith <Randall.Smith@amd.com>
2025-02-14 05:32:24 +0000

    switch _k_scale_float/_v_scale_float
    Signed-off-by: Randall Smith <Randall.Smith@amd.com>

--- vllm/attention/backends/abstract.py
+++ vllm/attention/backends/abstract.py
@@ -234,8 +234,8 @@ class AttentionLayer(Protocol):
 
     _k_scale: torch.Tensor
     _v_scale: torch.Tensor
-    _k_scale_float: torch.Tensor
-    _v_scale_float: torch.Tensor
+    _k_scale_float: float
+    _v_scale_float: float
     _q_scale: torch.Tensor
     _prob_scale: torch.Tensor
 
