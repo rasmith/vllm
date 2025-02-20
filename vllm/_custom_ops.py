0a80ed9a902da0818d99405d4fbec8bdfdb36010
Randall Smith <Randall.Smith@amd.com>
2025-02-20 03:04:39 +0000

    remove cast from fused_add_rms_norm
    Signed-off-by: Randall Smith <Randall.Smith@amd.com>

--- vllm/_custom_ops.py
+++ vllm/_custom_ops.py
@@ -158,9 +158,7 @@ def rms_norm(out: torch.Tensor, input: torch.Tensor, weight: torch.Tensor,
 
 def fused_add_rms_norm(input: torch.Tensor, residual: torch.Tensor,
                        weight: torch.Tensor, epsilon: float) -> None:
-    torch.ops._C.fused_add_rms_norm(
-            input.to(torch.float) if input.dtype == torch.float8_e4m3fnuz else input,
-            residual, weight, epsilon)
+    torch.ops._C.fused_add_rms_norm(input, residual, weight, epsilon)
 
 
 def advance_step_flashattn(num_seqs: int, num_queries: int, block_size: int,
